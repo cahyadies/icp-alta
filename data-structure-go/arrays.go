d3abb9c119d05e0d8cde07915e6a480d59b2b673
Eko Sugeng Cahyadi <ekosugengcahyadi@gmail.com>
2021-12-16 20:01:29 +0700

    First Commit

---
+++ data-structure-go/arrays.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var countries [3]string
+	countries[0] = "Indonesia"
+	countries[1] = "Malaysia"
+	countries[2] = "Singapura"
+
+	fmt.Println(countries)
+}
